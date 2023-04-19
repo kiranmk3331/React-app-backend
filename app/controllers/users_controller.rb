e54bdebdde0db274b8eda03a596e99f6fa08a069
“kiranmk3331” <kiranmk3331@gmail.com>
2023-04-19 17:25:00 +0530

    Add user create.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name, :user_name, :email, :password, :password_confirmation
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:create]
   post "/auth/login", to: "authentication#login"
   get "/*a", to: "application#not_found"
 end
